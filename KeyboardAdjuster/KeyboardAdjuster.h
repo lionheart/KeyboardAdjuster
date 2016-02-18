1ac72cc57dec94e2944fa7311aca03742edecfb5
Dan Loewenherz <dloewenherz@gmail.com>
2016-02-18 11:50:42 -0600

    add Xcode project

---
+++ KeyboardAdjuster/KeyboardAdjuster.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardAdjuster.h
+//  KeyboardAdjuster
+//
+//  Created by Daniel Loewenherz on 2/18/16.
+//  Copyright © 2016 Lionheart Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardAdjuster.
+FOUNDATION_EXPORT double KeyboardAdjusterVersionNumber;
+
+//! Project version string for KeyboardAdjuster.
+FOUNDATION_EXPORT const unsigned char KeyboardAdjusterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardAdjuster/PublicHeader.h>
+
+
