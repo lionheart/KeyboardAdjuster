ad2780445a852393be8776ef0d821e4c6f627c3c
Dan Loewenherz <dloewenherz@gmail.com>
2016-02-18 11:47:34 -0600

    remove example project

--- Example/Pods/Target Support Files/KeyboardAdjuster/KeyboardAdjuster-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KeyboardAdjuster : NSObject
-@end
-@implementation PodsDummy_KeyboardAdjuster
-@end
--- Example/Pods/Target Support Files/Pods-KeyboardAdjuster_Tests/Pods-KeyboardAdjuster_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_KeyboardAdjuster_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_KeyboardAdjuster_TestsVersionString[];
-
