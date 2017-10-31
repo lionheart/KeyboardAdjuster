bc33ad3d7133f3842cffcacfc972b50c120e44ca
Dan Loewenherz <dan@lionheartsw.com>
2017-10-30 20:35:30 -0500

    clean up bump version script

--- bump_version.sh
+++ bump_version.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Copyright 2016-2017 Lionheart Software LLC
 #
@@ -14,12 +14,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Usage: ./bump_version.sh 3.6.4
+
 if [ "$1" != "" ]; then
-  sed -i "" "s/\(s.version[ ]*=[ ]\).*/\1 \"$1\"/g" KeyboardAdjuster.podspec
-  git add .
-  git commit -m "bump version to $1"
-  git tag $1
-  git push origin master
-  git push --tags
-  pod trunk push
+    pod spec lint --quick
+
+    sed -i "" "s/\(s.version[ ]*=[ ]\).*/\1 \"$1\"/g" KeyboardAdjuster.podspec
+    sed -i "" "s/tree\/[\.0-9]*/tree\/$1/g" .jazzy.yaml
+
+    sh generate_docs.sh
+
+    git add .
+    git commit -m "bump version to $1"
+
+    # Delete tag if already exists
+    git tag -d $1
+    git push origin master :$1
+
+    git tag $1
+    git push origin master
+    git push --tags
+    pod trunk push
 fi
