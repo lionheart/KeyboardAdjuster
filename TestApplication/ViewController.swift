586047aeb481eb0ec3406f3d09c8d0fd5c9ec78c
Dan Loewenherz <dan@lionheartsw.com>
2018-09-29 17:30:37 -0500

    migrate to current swift syntax

--- TestApplication/ViewController.swift
+++ TestApplication/ViewController.swift
@@ -41,7 +41,7 @@ class ViewController: UIViewController, KeyboardAdjusterOptions {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.estimatedRowHeight = 44
-        tableView.rowHeight = UITableViewAutomaticDimension;
+        tableView.rowHeight = UITableView.automaticDimension;
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: CellIdentifier)
 
         view.addSubview(tableView)
